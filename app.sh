2dac9fce0fcc398f129e00725c3cbfc51994f6f3
root <root@0fcb807d8660>
2023-02-23 12:14:00 +0000

    Don't expose yourself, be careful

--- app.sh
+++ app.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #Get the file
-gdown https://drive.google.com/uc?id=1YCKctjZ64XUaKS9VFYdfG-BqSWxU1Ubu -O "/root/flag/encrypted/"
+host="Drive_URL"
+gdown $Drive_URL -O "/root/flag/encrypted/"
 # Set the file paths
 file_path="/root/flag/encrypted/flag.txt.gpg"
-password_file=".password"
+password_file="/Path/to/your/password/file"
 
 password=$(sed -n 1p $password_file)
 
